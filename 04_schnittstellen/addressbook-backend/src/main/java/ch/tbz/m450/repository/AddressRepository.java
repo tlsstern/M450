1eb97792abc2138c0fb70f1b7044bac6c1b366a8
tlsstern <tl@stern.ch>
2026-09-15 15:34:13 +0200

    Extracted zip in schnittstellen

--- 04_schnittstellen/addressbook-backend/src/main/java/ch/tbz/m450/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.tbz.m450.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-}
